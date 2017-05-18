3d923c16e78d342ce91d750ed4cb8ef12af0b1f1
Shawn <289904316@qq.com>
2017-05-18 17:40:20 +0800

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,20 @@
 /**
  * Created by Shawn on 2017/5/17.
  */
+var Chat = function (socket) {
+    this.socket = socket;
+};
+Chat.prototype.sendMessage = function (room, text) {
+    var message = {
+        room: room,
+        text: text
+    }
+    this.socket.emit('message', message);
+};
+Chat.prototype.processCommand = function (room) {
+    this.socket.emit('join', {
+        newRoom: room
+    });
+};
+
+
